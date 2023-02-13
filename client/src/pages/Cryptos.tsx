878a5bcf49a18faa7767303e7fb29ce1469296a7
Canaan Hanley <canaanhanley@gmail.com>
2023-02-13 13:30:11 -0500

    Added some personal space

--- client/src/pages/Cryptos.tsx
+++ client/src/pages/Cryptos.tsx
@@ -3,7 +3,7 @@ import Account from '../components/Account'
 
 export const Cryptos = () => {
   return (
-    <div className='bg-coal-800 w-full h-full'>
+    <div className='bg-coal-800 w-full h-screen'>
       <div className='flex flex-row justify-between px-10 py-10 text-3xl'>
         <div className='text-blue-300 font-light'>
         Cryptos
