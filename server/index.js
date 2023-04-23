aa3119db392410087be442500ad3bf077981d1c3
Robert Recalo <rrecalo3@ggc.edu>
2023-04-22 22:29:42 -0400

    Change express import in index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const uuid = require("uuid");
 const app = express();
+//
 const mongoose = require('mongoose')
 const UserModel = require('./models/User')
 const WagerModel = require('./models/Wager')
